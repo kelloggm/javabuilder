b6a63887ea58f0589e95a06e5164754639a579a7
Jessica Kulwik <jessica@code.org>
2021-02-11 23:24:11 +0000

    Added docker file

--- org-code-fargate-javacodebuilder/app/src/main/java/org/code/fargate/javacodebuilder/App.java
+++ org-code-fargate-javacodebuilder/app/src/main/java/org/code/fargate/javacodebuilder/App.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
@@ -20,7 +19,7 @@ public class App {
     return "Hello World!";
   }
 
-  public static String buildUserProject(Map<String, String> userProject) {
+  public static String buildUserProject(Map<?, ?> userProject) {
     // TODO: CSA-48 Handle more than one file
     if (userProject.size() > 1) {
       return "Error: Can only process one file at a time.";
@@ -28,12 +27,12 @@ public class App {
 
     File compileRunScript = null;
     String tempDir = System.getProperty("java.io.tmpdir");
-    for (Map.Entry<String, String> projectFile : userProject.entrySet()) {
-      String className = FilenameUtils.removeExtension(projectFile.getKey());
-      String userCode = projectFile.getValue();
+    for (Map.Entry<?, ?> projectFile : userProject.entrySet()) {
+      String className = FilenameUtils.removeExtension((String) projectFile.getKey());
+      String userCode = (String) projectFile.getValue();
       try {
         // Build user's java file
-        File userFile = new File(Paths.get(tempDir, projectFile.getKey()).toString());
+        File userFile = new File(Paths.get(tempDir, (String) projectFile.getKey()).toString());
         FileWriter writer = new FileWriter(userFile);
         writer.write(userCode);
         writer.close();
@@ -88,17 +87,14 @@ public class App {
     JSONParser parser = new JSONParser();
     JSONObject projectData = null;
     try {
-      projectData = (JSONObject) parser.parse(new FileReader("src/main/resources/sampleInput.json"));
+      // Right now this works if the jar is invoked from the directory ../../../../../../.. - i.e. the app directory
+      projectData = (JSONObject) parser.parse(new FileReader("/tmp/userProgram.json"));
     } catch (IOException | ParseException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
       System.out.println("Error parsing json file");
     }
 
-    var userProject = new HashMap<String, String>();
-
-
-    userProject.put("Foo.java", "public class Foo {\n  public static void main(String[] args) {\n    System.out.println(\"Hello Molly!\");\n  }\n}\n");
     System.out.println(App.buildUserProject(projectData));
   }
 }
