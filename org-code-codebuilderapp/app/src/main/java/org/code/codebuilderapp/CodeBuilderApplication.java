e4a34fdf704c8951062ca646b9e5b7f1ee204ad0
Jessica Kulwik <jessica@code.org>
2021-02-16 01:49:20 +0000

    Added more notes

--- org-code-codebuilderapp/app/src/main/java/org/code/codebuilderapp/CodeBuilderApplication.java
+++ org-code-codebuilderapp/app/src/main/java/org/code/codebuilderapp/CodeBuilderApplication.java
@@ -8,9 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // made following instructions here: https://spring.io/guides/gs/messaging-stomp-websocket/
 // and here: https://www.mokkapps.de/blog/sending-message-to-specific-anonymous-user-on-spring-websocket/
+// and using this discussion on principal https://stackoverflow.com/questions/37853727/where-user-comes-from-in-convertandsendtouser-works-in-sockjsspring-websocket
 @SpringBootApplication
 public class CodeBuilderApplication {
     public static void main(String[] args) {
         SpringApplication.run(CodeBuilderApplication.class, args);
     }
 }
+
+/* Sample test code
+public class Foo {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+*/
